76fdb5b8c4f0bffdaa7244d8fe0c55da7eaf87cb
Digo-Castro <dygo.castro@gmail.com>
2021-01-14 19:46:25 -0300

    Modulo 2 Bloco 11 Dia 2: Exercicio -> Pokedex - Part I

---
+++ Modulo_2/BLOCO_11/DIA_2/pokedex/src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <p />
+      </main>
+    );
+  }
+}
+
+export default App;
